f632586586a3ef8c7ada73a2251dcfb26aba98aa
MaryamSiddiqui-ms <ms.maryamsiddiqui@.com>
2023-11-30 19:06:43 +0500

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      
+      <h1>zkVML</h1>
+      <h2>
+      Zero-Knowledge Validating Machine Learning Model
+      </h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <button>
+          Input data 
         </button>
+        <button>
+          Generate Proof
+        </button>
+        <button>
+          Verify
+        </button>
+      
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+       Click on logo to view the documentation
       </p>
     </>
   )
